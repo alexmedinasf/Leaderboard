799465e7084fa94a637cc3ddfea6106c862a6de9
alexmedinasf <alexsales001@hotmail.com>
2022-10-22 17:40:15 -0300

    linters fix

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'development',
-  entry: './src/index.js',
-devServer: {
-static: './dist',
-}
+  devServer: {
+    static: './dist',
+  },
 };
