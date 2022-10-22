553ddf6d477eb41db4b04a8b4a2af01ae1d9390e
alexmedinasf <alexsales001@hotmail.com>
2022-10-22 18:48:11 -0300

    fixes

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import addScore from './modules/addScores.js';
 import getScores from './modules/getScores.js';
 import './style.css';
 
-const scoresList = document.querySelector('.scores-list');
+const scoresList = document.querySelector('#leaderboard');
 const submitButton = document.querySelector('#submit-score');
 const refreshButton = document.querySelector('#refresh');
-const message = document.querySelector('.message');
 
 refreshButton.addEventListener('click', (e) => {
   e.preventDefault();
@@ -16,5 +15,5 @@ submitButton.addEventListener('click', (e) => {
   e.preventDefault();
   const user = document.querySelector('.user').value;
   const score = document.querySelector('.score').value;
-  addScore(scoresList, user, score, message);
+  addScore(scoresList, user, score);
 });
