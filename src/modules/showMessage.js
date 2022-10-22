126dc2f4f1b3490aa59e367f15a77c612f1adbee
alexmedinasf <alexsales001@hotmail.com>
2022-10-22 16:32:26 -0300

    linters

--- src/modules/showMessage.js
+++ src/modules/showMessage.js
@@ -1,11 +1,10 @@
 const showMessage = (message, classToRemove, classToAdd, innerHTML) => {
-    message.classList.remove(classToRemove);
-    message.classList.add(classToAdd);
-    message.innerHTML = innerHTML;
-    setTimeout(() => {
-      message.classList.remove(classToAdd);
-      message.innerHTML = '';
-    }, 3000);
-  };
-  
-  export default showMessage;
+  message.classList.remove(classToRemove);
+  message.classList.add(classToAdd);
+  message.innerHTML = innerHTML;
+  setTimeout(() => {
+    message.classList.remove(classToAdd);
+    message.innerHTML = '';
+  }, 3000);
+};  
+export default showMessage;
